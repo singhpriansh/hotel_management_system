0581378fbdb414261ee4de0092c93099e3f4f484
singhpriansh <priyanshu.h786@gmail.com>
2023-08-19 15:21:24 +0530

    first_page_made

--- src/main/java/com/hotel/management/system/hotel_management_system.java
+++ src/main/java/com/hotel/management/system/hotel_management_system.java
@@ -1,18 +1,52 @@
 package com.hotel.management.system;
 
-import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Font;
 
-// import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.swing.*;
 
-// @SpringBootApplication
-public class hotel_management_system extends JFrame{
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class hotel_management_system extends JFrame {
 
 	hotel_management_system() {
-		setSize(1366, 565);
-    setLocation(100, 100);
-		
-    setVisible(true);
+		// setSize(1366, 565);
+    // setLocation(100, 100);
+		setBounds(100, 100, 1366, 565);
+
+		ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/first.jpg"));
+		JLabel image = new JLabel(i1);
+		add(image);
+
+		JLabel text = new JLabel("HOTEL MANAGEMENT SYSTEM");
+		text.setBounds(20, 430, 1000, 35);
+		text.setForeground(Color.WHITE);
+		text.setFont(new Font("serif", Font.PLAIN, 50));
+		image.add(text);
+
+		JButton next = new JButton("Next");
+		next.setBounds(1150,450,150,50);
+		next.setBackground(Color.WHITE);
+		next.setForeground(Color.MAGENTA);
+		next.setFont(new Font("serif", Font.PLAIN, 24));
+		image.add(next);
+		setVisible(true);
+
+		while(true) {
+			text.setVisible(false);
+			try {
+				Thread.sleep(500);
+			} catch (Exception e){
+				e.printStackTrace();
+			}
+			text.setVisible(true);
+			try {
+				Thread.sleep(500);
+			} catch (Exception e){
+				e.printStackTrace();
+			}
+		}
 	}
 
 	public static void main(String[] args) {
