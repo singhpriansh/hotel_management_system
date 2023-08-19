a77184383e0dca5ff61428765651fc98d704f29b
singhpriansh <priyanshu.h786@gmail.com>
2023-08-19 16:14:29 +0530

    login_page_made

---
+++ src/main/java/com/hotel/management/system/Login.java
@@ -0,0 +1,54 @@
+package com.hotel.management.system;
+
+import java.awt.Color;
+import java.awt.Image;
+
+import javax.swing.*;
+
+public class Login extends JFrame{
+  Login () {
+    getContentPane().setBackground(Color.WHITE);
+    setLayout(null);
+
+    JLabel user = new JLabel("Username");
+    user.setBounds(40, 20, 100, 30);
+    add(user);
+
+    JTextField username = new JTextField();
+    username.setBounds(150, 20, 150, 30);
+    add(username);
+
+    JLabel pass = new JLabel("Password");
+    pass.setBounds(40, 70, 100, 30);
+    add(pass);
+
+    JTextField password = new JTextField();
+    password.setBounds(150, 70, 150, 30);
+    add(password);
+
+    JButton login = new JButton("Login");
+    login.setBounds(40,150,100,30);
+    login.setBackground(Color.WHITE);
+    login.setForeground(Color.BLACK);
+    add(login);
+
+    JButton cancel = new JButton("Cancel");
+    cancel.setBounds(180,150,100,30);
+    cancel.setBackground(Color.WHITE);
+    cancel.setForeground(Color.BLACK);
+    add(cancel);
+
+    ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/second.jpg"));
+    Image i2 = i1.getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT);
+    ImageIcon i3 = new ImageIcon(i2);
+    JLabel image = new JLabel(i3);
+    image.setBounds(350,10,200,200);
+    add(image);
+
+    setBounds(500,200,600,300);
+    setVisible(true);
+  }
+  public static void main(String[] args) {
+    new Login();
+  }
+}
--- src/main/java/com/hotel/management/system/hotel_management_system.java
+++ src/main/java/com/hotel/management/system/hotel_management_system.java
@@ -2,13 +2,14 @@ package com.hotel.management.system;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class hotel_management_system extends JFrame {
+public class hotel_management_system extends JFrame implements ActionListener {
 
 	hotel_management_system() {
 		// setSize(1366, 565);
@@ -29,6 +30,7 @@ public class hotel_management_system extends JFrame {
 		next.setBounds(1150,450,150,50);
 		next.setBackground(Color.WHITE);
 		next.setForeground(Color.MAGENTA);
+		next.addActionListener(this);
 		next.setFont(new Font("serif", Font.PLAIN, 24));
 		image.add(next);
 		setVisible(true);
@@ -52,5 +54,12 @@ public class hotel_management_system extends JFrame {
 	public static void main(String[] args) {
 		// SpringApplication.run(HotelManagementApplication.class, args);
 		new hotel_management_system();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		setVisible(false);
+		new Login();
+		// throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
 	}
 }
